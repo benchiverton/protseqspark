f87dd768f90a980e67f0ce4e45fe13ab93bf76d1
Ben Chiverton <benc@calastone.com>
2020-09-27 10:12:27 +0100

    test linters

--- protseqspark/ProtSeqIO/fasta_helper.py
+++ protseqspark/ProtSeqIO/fasta_helper.py
@@ -4,6 +4,6 @@ from ..ProtSeq import ProteinSequence
 
 def readSequencesFromFasta(fasta_file: str) -> Iterator[ProteinSequence]:
     for seq_record in SeqIO.parse(fasta_file, "fasta"):
-        header = str(seq_record.id).split('|')
+         header = str(seq_record.id).split('|')
         accessionSpecie = str(header[2]).split("_")
         yield ProteinSequence(header[1], accessionSpecie[0], accessionSpecie[1], seq_record.seq)
