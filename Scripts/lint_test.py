2d9cdb6a2c74d4d9258f0a97c63fb5eadfc287f7
Ben Chiverton <benc@calastone.com>
2020-09-27 12:11:07 +0100

    Create lint_test.py

---
+++ Scripts/lint_test.py
@@ -0,0 +1,6 @@
+class Test:  
+    def __init__(self, accession: str, geneName: str, specie: str,sequence: str):
+        self.accession = accession
+        self.geneName = geneName
+        specie = specie  
+        self.sequence = sequence
