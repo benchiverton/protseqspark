b656ea0815cecfaf43841ce8d136b5b9d42fd985
benchiverton <benjichiverton@gmail.com>
2021-02-15 18:07:59 +0000

    Cleanup, linting + more tests

--- Tests/test_pybam.py
+++ Tests/test_pybam.py
@@ -1,14 +1,21 @@
 """Tests for pybam module."""
 
-from pybam import BamFile, BamEntry, PybamWarn
-from typing import Iterator
+from pybam import BamFile, PybamWarn
 import pytest
+
 
 class TestRead:
     def test_getBamRecordsFromBamFile1Correctly(self):
         bf = BamFile('Tests/Bam/bam1.bam')
         for sequence in bf.getBamRecords():
-            assert sequence.bam_qname.decode("utf-8").startswith('HWI-1KL120:88:D0LRBACXX:1:1101:') == True
+            assert sequence.bam_qname.decode("utf-8").startswith('HWI-1KL120:88:D0LRBACXX:1:1101:')
+
+    def test_getFirstSequenceFromFile1Correctly(self):
+        bf = BamFile('Tests/Bam/bam1.bam')
+        records = bf.getBamRecords()
+        record = next(records)
+        # the known value of the first sequence
+        assert record.sam_seq == 'GAGAGGTCAGCGTGAGCCCCTTGCCTCACACCGGCCCCTCTCACGCCGAGAGAGGTCAGCGTGAGCCCCTTGCCTCACACCGGCCCCTCCCACGCCGAGAG'
 
     def test_getFileChromosomesFromFile1Correctly(self):
         bf = BamFile('Tests/Bam/bam1.bam')
@@ -18,7 +25,14 @@ class TestRead:
     def test_getBamRecordsFromBamFile2Correctly(self):
         bf = BamFile('Tests/Bam/bam2.bam')
         for sequence in bf.getBamRecords():
-            assert sequence.bam_qname.decode("utf-8").startswith('HWI-1KL120:88:D0LRBACXX:1:1101:') == True
+            assert sequence.bam_qname.decode("utf-8").startswith('HWI-1KL120:88:D0LRBACXX:1:1101:')
+
+    def test_getFirstSequenceFromFile2Correctly(self):
+        bf = BamFile('Tests/Bam/bam2.bam')
+        records = bf.getBamRecords()
+        record = next(records)
+        # the known value of the first sequence
+        assert record.sam_seq == 'GTTGTTTCTTCCTTTTTTCCCCTCTTCCCCTTCCTGTCTTGTGTTTTTTCTGAACCCCACATCTGCCATCATTTCTCTCCTCTAGAGTCCCTTGCTTCTGC'
 
     def test_getFileChromosomesFromFile2Correctly(self):
         bf = BamFile('Tests/Bam/bam2.bam')
